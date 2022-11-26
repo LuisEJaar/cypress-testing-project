50c568d71b25018ac66331e5cc187ed46c47a2c0
LuisEJaar <luisjaarcoding@gmail.com>
2022-11-26 12:42:23 -0600

    Integrating fixtures

--- cypress/integration/examples/login.spec.js
+++ cypress/integration/examples/login.spec.js
@@ -1,22 +1,50 @@
 describe('working with inputs', () => {
+	it('should override the current time', () => {
+		const date = new Date(2020, 3, 10).getTime(); //return a time stamp
+		//this will hard code the times in cypress to this
+		cy.clock(date);
+		cy.log(date);
+	});
 
-  it('Should fill username', () => {
-    cy.visit('http://zero.webappsecurity.com/login.html')
-    cy.get('#user_login').clear();
-    cy.get('#user_login').type('Some Invalid Name');
-  })
-  
-  it('Should fill password', () => {
-    cy.get('#user_password').clear();
-    cy.get('#user_password').type('Some Invalid password');
-  })
+	it('Should load login page', () => {
+		cy.visit('http://zero.webappsecurity.com/login.html');
+		//not needed as cypress auto clears cookies upon new tests, but just in case:
+		cy.clearCookies({ log: true });
+		cy.clearLocalStorage('your item', { log: true });
 
-  it('Should submit login form', () => {
-    cy.contains('Sign in').click();
-  })
+		cy.title().should('include', 'Zero - Log in');
+	});
 
-  it('Should display error message', () => {
-    cy.get('.alert-error').should('be.visible')
-  })
-  
-})
+	it('Should fill username', () => {
+		//Example of creating an alias for a selector
+		cy.get('#user_login').as('username'); //Setting the id to the alias
+		cy.get('@username').clear(); //using the alias
+		cy.get('@username').type('Some Invalid Name');
+
+		//Example of additional input for type (a delay which translates to a slower typing speed)
+		// cy.get('#user_login').type('Some Invalid Name', {delay: 50});
+	});
+
+	it('Should fill password', () => {
+		cy.get('#user_password').as('password');
+		cy.get('@password').clear();
+		cy.get('@password').type('Some Invalid password');
+	});
+
+	it('Should mark checkbox', () => {
+		//Good for checkboxes and radios
+		cy.get('input[type="checkbox"]').click();
+		// You can wait / pause at any time
+		// cy.wait(5000);
+	});
+
+	it('Should submit login form', () => {
+		cy.contains('Sign in').click();
+	});
+
+	it('Should display error message', () => {
+		cy.get('.alert-error')
+			.should('be.visible')
+			.and('contain', 'Login and/or password are wrong.');
+	});
+});
