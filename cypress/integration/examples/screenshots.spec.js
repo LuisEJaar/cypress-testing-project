31e14b266aacf55d18cd3f74aa28edf4225ecb35
LuisEJaar <luisjaarcoding@gmail.com>
2022-11-29 06:31:35 -0600

    implemented screenshot logic

--- cypress/integration/examples/screenshots.spec.js
+++ cypress/integration/examples/screenshots.spec.js
@@ -1,9 +1,10 @@
 describe('Taking screenshots', ()=> {
     it('full page screenshot', ()=> {
-
+        cy.visit('https://devexpress.github.io/testcafe/example/')
+        cy.screenshot({capture: 'fullPage'})
     })
 
     it('single element screenshot', ()=> {
-
+        cy.get('header').screenshot()
     })
 })
