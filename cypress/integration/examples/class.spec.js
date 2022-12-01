54d74d1ed4aa80c1933b87175e61d24213d722a2
LuisEJaar <luisjaarcoding@gmail.com>
2022-12-01 10:57:14 -0600

    learning about cypress hooks

--- cypress/integration/examples/class.spec.js
+++ cypress/integration/examples/class.spec.js
@@ -19,9 +19,32 @@ class HomePage extends BasePage {
 	}
 }
 
+
 describe('Abstraction with classes', () => {
+  before(function () {
+    //runs before all it tests
+    //setup test data or test context
+    //seed or reset the database
+    HomePage.loadHomePage();
+  })
+
+  after(function () {
+    //runs after all it steps finish
+    //test clean up
+    //clean cookies or localStorage
+  })
+
+  beforeEach(function () {
+    //runs before each individual it block
+    
+  })
+
+  afterEach(function () {
+    //runs after each it block in the describe
+    
+  })
+
 	it('should scroll down and up on the page', () => {
-		HomePage.loadHomePage();
 		HomePage.scrollToBottom();
 		HomePage.wait(3000);
     HomePage.scrollToTop();
