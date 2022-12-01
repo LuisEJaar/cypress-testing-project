b7564580c5c5e272c2c07fbc8107d86e5fa38003
LuisEJaar <luisjaarcoding@gmail.com>
2022-12-01 09:57:55 -0600

    learned about abstracting with class based tests

---
+++ cypress/integration/examples/class.spec.js
@@ -0,0 +1,30 @@
+//Normally you would create separate folders for all classes
+class BasePage {
+	static loadHomePage() {
+		cy.visit('https://devexpress.github.io/testcafe/example/');
+	}
+
+	static wait(number) {
+		cy.wait(number);
+	}
+}
+
+class HomePage extends BasePage {
+	static scrollToBottom() {
+		cy.get('#submit-button').scrollIntoView();
+	}
+
+	static scrollToTop() {
+		cy.get('header').scrollIntoView();
+	}
+}
+
+describe('Abstraction with classes', () => {
+	it('should scroll down and up on the page', () => {
+		HomePage.loadHomePage();
+		HomePage.scrollToBottom();
+		HomePage.wait(3000);
+    HomePage.scrollToTop();
+    HomePage.wait(2000);
+	});
+});
--- cypress/integration/examples/data.spec.js
+++ cypress/integration/examples/data.spec.js
@@ -12,10 +12,20 @@ describe('Write / Read Data to JSON / Text file', () => {
 
 	it('should read and verify data from json file', () => {
 		cy.readFile('log.json').its('age').should('eq', 12);
-  });
-  
-  it('should read and verify data from txt file', () => {
-    cy.readFile('log.txt').should('contain', 'Hello');
-    cy.readFile('log.txt').should('eq','Hello');
-  });
+	});
+
+	it('should read and verify data from txt file', () => {
+		cy.readFile('log.txt').should('contain', 'Hello');
+		cy.readFile('log.txt').should('eq', 'Hello');
+	});
+
+	it('should read and verify browser document content', () => {
+		cy.visit('https://www.example.com');
+		//need to wait for pageload
+		cy.wait(2000);
+
+		cy.document().its('contentType').should('eq', 'text/html');
+
+		cy.document().should('have.property', 'charset').and('eq', 'UTF-8');
+	});
 });
