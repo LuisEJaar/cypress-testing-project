1472e5807bceb4eadeb6465ccbee9036a73192f0
LuisEJaar <luisjaarcoding@gmail.com>
2022-12-01 09:29:53 -0600

    added reading from json / text files

--- cypress/integration/examples/data.spec.js
+++ cypress/integration/examples/data.spec.js
@@ -1,12 +1,21 @@
 describe('Write / Read Data to JSON / Text file', () => {
-  it('Should write data', () => {
-    cy.writeFile('log.json', {
-      name: 'Mike',
-      age: 12
-    })
-  })
+	it('Should write data', () => {
+		cy.writeFile('log.json', {
+			name: 'Mike',
+			age: 12,
+		});
+	});
 
-  it('Should write to text file', () => {
-    cy.writeFile('log.txt', 'Hello')
-  })
-})
+	it('Should write to text file', () => {
+		cy.writeFile('log.txt', 'Hello');
+	});
+
+	it('should read and verify data from json file', () => {
+		cy.readFile('log.json').its('age').should('eq', 12);
+  });
+  
+  it('should read and verify data from txt file', () => {
+    cy.readFile('log.txt').should('contain', 'Hello');
+    cy.readFile('log.txt').should('eq','Hello');
+  });
+});
